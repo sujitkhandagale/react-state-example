7551b4bef97ffc448e00e96a50bcebfd56ee8f0f
sujitkhandagale <Sujitkh94@outlook.com>
2023-03-18 12:12:15 +0530

    few changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Screenshot from "./Screenshot.png";
 
 function App() {
-  let count = 0;
+  const [count, setCount] = useState(0);
 
-  function HandleInput() {
-    count+=1;
-    console.log(count);
+  function HandleAddition() {
+    setCount(count + 1);
   }
+
+  function HandleSubtraction() {
+    setCount(count - 1);
+  }
+
   return (
-    <div className="App">
-<p>{count}</p>
-<button className='btn_green' onClick={HandleInput}>Add + 1</button>
-&nbsp;
-<button className='btn_red' onClick={HandleInput}>Remove + 1</button>
-<h1>Note</h1>
-<span>React js Have Strong Approch On </span>
+    <>
+      <div className="App">
+        <p>{count}</p>
+        <button className="btn_green" onClick={HandleAddition}>
+          Add + 1
+        </button>
+        &nbsp;
+        <button className="btn_red" onClick={HandleSubtraction}>
+          Remove + 1
+        </button>
+        <h1>Note</h1>
+      </div>
+    <div className="description_note">
+    <div className="description">
+        <h2>
+          <span>Simple Defination</span> : State Is Build-in React object That
+          contain information or data of Components. whenever changes made.
+          react re-render the component.
+        </h2>
+        <div className="code">
+         <div> <img src={Screenshot} alt="" /></div>
+         <div>
+
+         </div>
+        </div>
+      </div>
     </div>
+    </>
   );
 }
 
